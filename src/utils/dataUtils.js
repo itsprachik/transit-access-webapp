4785510554724da7b140c7f95f1f8c96775a799d
Joel Chapman <joelaaron91@gmail.com>
2024-10-01 10:23:43 -0400

    - added geo control and zoom levels - added EL number - CSS styling to include blur and transparency - icons for elevator outages

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,3 +1,7 @@
+/* 
+dataUtils is used to filter existing arrays and elevator data
+*/
+
 import { elevatorCoordinates } from "./elevatorOutageGeometry";
 
 // Function to filter out Escalators and Upcoming outages from the outageArray response
@@ -24,4 +28,33 @@ export default function getElevatorOutages(outageArray) {
     }
   });
   return features;
+}
+
+// Function to highlight all upcoming outages
+export function getUpcomingOutages(outageArray) {
+  const upcomingOutages = [];
+  outageArray.forEach((equip) => {
+    if (equip.equipmenttype == "EL" && equip.isupcomingoutage == "Y") {
+      let obj = {
+        type: "Feature",
+        id: equip.equipment,
+        properties: {
+          station: equip.station,
+          outagedate: equip.outagedate,
+          returntoservice: equip.estimatedreturntoservice,
+          elevatorno: equip.equipment,
+          isBroken: true,
+        },
+        // Check if co-ordinates exist for elevator no, if not assign null. This keeps this layer from erroring out if an elevator is returned as out by the mta but that is not currently in our map.
+        geometry: elevatorCoordinates[equip.equipment]
+          ? {
+              coordinates: elevatorCoordinates[equip.equipment],
+              type: "Point",
+            }
+          : null,
+      };
+      upcomingOutages.push(obj);
+    }
+  });
+  return upcomingOutages;
 }
