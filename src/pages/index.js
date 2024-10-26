4c3f960e9ae2e436d64c96414c75c409ac79ff23
itsprachik <itsprachik@gmail.com>
2024-10-25 23:56:32 -0400

    Added subway line icons to onclick and onhover popups

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable @next/next/no-sync-scripts */
-// pages/index.js
 import Head from 'next/head';
-import Map from '@/components/map';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from '@/components/Map';
 
 
 export default function Home() {
@@ -12,9 +11,7 @@ export default function Home() {
         <title>Transit Access</title>
         <meta name="description" content="A Next.js app with Mapbox" />
         <link rel="icon" href="/favicon.ico" />
-
       </Head>
-
       <main>
         <Map />
       </main>
---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+import { A, B, C, D, E, F, G, J, L, Line1, Line2, Line3, Line4, Line5, Line6, Line7, M, N, Q, R, S, W, Z } from "@/components/icons/MtaLineIcons";
+
+export const MTA_SUBWAY_LINE_ICONS = {
+    1:  <Line1 />,
+    2: <Line2 />,
+    3: <Line3 />,
+    4: <Line4 />,
+    5: <Line5 />,
+    6: <Line6 />,
+    7: <Line7 />,
+    A: <A />,
+    B: <B />,
+    C: <C/>,
+    D: <D />,
+    E: <E />,
+    F: <F />,
+    G: <G />,
+    J: <J />,
+    L: <L />,
+    M: <M />,
+    N: <N />,
+    Q: <Q />,
+    R: <R />,
+    S: <S />,
+    W: <W />,
+    Z: <Z />
+
+}
