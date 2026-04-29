f992ac94c8952105ed5d23e714a8afa67fbbc324
Joel Chapman <joelaaron91@gmail.com>
2026-04-29 14:58:50 -0400

    Added sitemap postbuild and data structures to help searchability

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://transitaccess.org',
+  generateRobotsTxt: true,
+}
+
+export default config;
